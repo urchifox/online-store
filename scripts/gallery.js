98995e99ff7f7915fa89923a9fecf34a494d3405
urchifox <126313096+urchifox@users.noreply.github.com>
2023-11-14 17:12:46 +0400

    make card-small js module

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,106 +1,30 @@
 import { ITEMS_DATA } from './mock/items-data.js';
 import { createElement } from './utils.js';
+import { render as renderCardSmall} from './card-small.js';
 
-const gallery = document.querySelector('.gallery');
-
-const pluralRules = new Intl.PluralRules('ru-RU');
-
-const getRewievsCountMessage = (number) => {
-	switch (pluralRules.select(number)) {
-		case 'one': {
-			return `${number.toLocaleString('ru-RU')} отзыв`;
-		}
-		case 'few': {
-			return `${number.toLocaleString('ru-RU')} отзыва`;
-		}
-		case 'many': {
-			return `${number.toLocaleString('ru-RU')} отзывов`;
-		}
-		// case 'other'
-		default: {
-			return `${number.toLocaleString('ru-RU')} отзыва`;
-		}
-	}
-};
-
-const getShevronTemplate = (discount, cashback, isHit) => {
-	if (discount > 0) {
-		return `<p class="card-small__shevron card-small__shevron_discount">Скидка ${discount}%</p>`;
-	}
-
-	if (cashback > 0) {
-		return `<p class="card-small__shevron card-small__shevron_cashback">Кэшбэк ${cashback}%</p>`;
-	}
-
-	if (isHit) {
-		return '<p class="card-small__shevron card-small__shevron_hit">Хит</p>';
-	}
-
-	return '';
-};
-
-const getPriceText = (price, discount) => {
-	if (discount > 0) {
-		const oldPrice = price;
-		const newPrice = price * (100 - discount) / 100;
-		return `<p class="card-small__price">
-					<span class="card-small__price-current">${newPrice.toLocaleString('ru-RU')}
-						<span class="visually-hidden">рублей</span>
-					</span>
-					<s class="card-small__price-old">${oldPrice.toLocaleString('ru-RU')}
-						<span class="visually-hidden">рублей</span>
-					</s>
-				</p>`;
-	}
-
-	return `<p class="card-small__price">
-				<span class="card-small__price-current">${price.toLocaleString('ru-RU')}
-					<span class="visually-hidden">рублей</span>
-				</span>
-			</p>`;
-};
-
-const getPicturesTemplate = (pictures) => pictures.reduce(
-	(accumulator, picture) => `${accumulator}<img class="card-small__cover" src="images/covers/${picture}" alt="">`, '');
-
-const getTemplate = (id, {type, brand, model, price, discount, cashback, isHit, isAvailable, rate, rewievsCount, colorOptions, pictures, memoryOptions}) => `
-	<li data-id="${id}" class="card-small gallery__card-small">
-		<h2 class="card-small__name">
-			<a href="#" class="card-small__name-link link">${type} ${brand} ${model} ${Math.max(...memoryOptions)}Gb, ${colorOptions[0].name}</a>
-		</h2>
-		${getShevronTemplate(discount, cashback, isHit)}
-		${getPriceText(price, discount)}
-		<div class="card-item__info">
-			<p class="card-small__availability">${isAvailable ? 'Есть' : 'Нет'} в наличии</p>
-			<p class="card-small__rate"><span class="visually-hidden">Рейтинг</span>${rate.toFixed(1)}</p>
-			<a href="#" class="card-small__reviews link">${getRewievsCountMessage(rewievsCount)}</a>
-		</div>
-		<a href="#" class="card-small__cover-link">
-			${getPicturesTemplate(pictures)}		
-		</a>
-		<div class="card-small__buttons">
-			<button class="card-small__buy button-solid">В корзину</button>
-			<button class="card-small__favourite button-outlined ">
-				<img src="images/icons/heart-icon.svg" alt="">
-				<span class="visually-hidden">Добавить в избранное</span>
-			</button>
-			<button class="card-small__compare button-outlined ">
-				<img src="images/icons/compare-icon.svg" alt="">
-				<span class="visually-hidden">Добавить к сравнению</span>
-			</button>
-		</div>
-	</li>`;
+const root = document.querySelector('main');
 
 const render = (cb) => {
+	const gallery = createElement(`
+		<article class="gallery-container">
+			<h1 class="visually-hidden">Каталог товаров</h1>
+			<ul class="gallery main__gallery"></ul>
+		</article>`);
+
 	const fragment = document.createDocumentFragment();
 
 	for (const [id, item] of ITEMS_DATA) {
-		const card = createElement(getTemplate(id, item));
+		const card = renderCardSmall(id, item);
 		card.addEventListener('click', cb);
 		fragment.append(card);
 	}
 
-	gallery.append(fragment);
+	gallery.querySelector('.gallery').append(fragment);
+	root.append(gallery);
 };
 
-export {render};
+const remove = () => {
+	document.querySelector('.gallery-container').remove();
+};
+
+export {render, remove};
